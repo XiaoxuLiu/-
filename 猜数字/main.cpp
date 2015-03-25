e949f814dbe981df3e876e2d7db44b0c2f54be43
XiaoxuLiu <liuxiaoxu@liuxiaoxudeMacBook-Air.local>
2015-03-25 18:21:55 +0800

    Initial Commit

---
+++ 猜数字/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  猜数字
+//
+//  Created by 刘晓旭 on 15/3/25.
+//  Copyright (c) 2015年 刘晓旭. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
